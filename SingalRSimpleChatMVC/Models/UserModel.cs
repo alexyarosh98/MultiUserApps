63ca16c637b6b0bfa7b39b2913d2ebf614774586
Alexander <alexyarosh98@gmail.com>
2018-02-24 13:43:57 +0300

    Add project files.

---
+++ SingalRSimpleChatMVC/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SingalRSimpleChatMVC.Models
+{
+    public class UserModel
+    {
+        public string ConnectionId { get; set; }
+        public string Name { get; set; }
+    }
+}
